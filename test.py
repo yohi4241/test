581c9fe1a7e88b4eca2b49132eff62d26acd07f6
yohi4241 <142667354+yohi4241@users.noreply.github.com>
2024-09-16 10:43:01 +0900

    test.py

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ os.system("pip install -U scratchattach")
 import scratchattach as scratch3
 
 session = scratch3.Session("eJxVj81qwzAQhN_F58aV9eO1cmtODQRDAyn0JNaSHCuOpCA7hLb03SuDL7nONzsz", username="midoriyohi") #The username field is case sensitive
-conn = session.connect_cloud("882454152")
+conn = session.connect_cloud("1068139990")
 conn.set_var("aaa", "1") #the variable name is specified without the cloud emoji
